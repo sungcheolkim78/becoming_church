06dcf8b462b309123742b8e730fc8a25773bf470
SungCheol Kim <asm4g.ksc@gmail.com>
2019-03-24 08:10:54 -0400

    add day 18, 37

--- gen_yaml_templ.sh
+++ gen_yaml_templ.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-filename=day$1.yaml
+filename=yaml_40days/day$1.yaml
 
 if [ -e $filename ]; then
   echo "... $filename exists!"
 else
   cat > $filename << EOF
 day: '$1'
-prayer: ''
-song: ''
-summary: ''
 title: ''
+song: ''
+prayer: ''
+summary: ''
 verses:
+- ''
+- ''
+- ''
+- ''
+- ''
 - ''
 - ''
 - ''
